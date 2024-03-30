e7857e634f5062c82f89c38d6ea7edc016589767
Jhoan Andres Diaz Castaño <115663029+JDiazc0@users.noreply.github.com>
2024-03-30 16:51:47 -0500

    Update products.controller.js
    get method

--- server/src/controllers/products.controller.js
+++ server/src/controllers/products.controller.js
@@ -2,7 +2,7 @@ import Products from "../models/products.model.js";
 
 export const getProducts = async (req, res) => {
   try {
-    const products = await Products.find().populate("materials._id");
+    const products = await Products.find().populate("materials");
     res.json(products);
   } catch (error) {
     res.status(500).json({ error: "Products error" });
