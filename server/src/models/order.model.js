d92bb545095f369f186eac86174d1c33bae89edd
Jhoan Andres Diaz Castaño <115663029+JDiazc0@users.noreply.github.com>
2024-05-30 18:20:53 -0500

    Update order.model.js

--- server/src/models/order.model.js
+++ server/src/models/order.model.js
@@ -5,10 +5,12 @@ const productSchema = new mongoose.Schema({
   product: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Product",
+    required: true,  
   },
   quantity: {
     type: Number,
     required: true,
+    min: 1,  
   },
 });
 
@@ -16,16 +18,25 @@ const orderSchema = new mongoose.Schema({
   client: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Client",
+    required: true, 
   },
-  products: [productSchema],
+  products: {
+    type: [productSchema],
+    validate: [arrayLimit, '{PATH} debe tener al menos un producto']  
+  },
   price: {
     type: Number,
     required: true,
+    min: 0,  
   },
   date: {
     type: Date,
     default: () => moment().tz("America/Bogota").toDate(),
   },
 });
+
+function arrayLimit(val) {
+  return val.length > 0;
+}
 
 export default mongoose.model("Order", orderSchema);
