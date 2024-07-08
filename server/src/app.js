e61dbe67afe0463e8b2f3a8f93237c2dcbb7c322
Jhoan Andres Diaz Castaño <115663029+JDiazc0@users.noreply.github.com>
2024-07-08 14:37:13 -0500

    cors configuration

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,8 @@ import balanceRouter from "./routes/balance.routes.js";
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'http://localhost:3000',
+    credentials: true
   }));  
 app.use(morgan("dev"));
 app.use(express.json());
