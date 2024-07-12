6b53d580bd27dc8b43ce80ad3e5d0bf44777a255
Jhoan Andres Diaz Castaño <115663029+JDiazc0@users.noreply.github.com>
2024-07-12 16:42:24 -0500

    Update balance.schema.js

--- server/src/schemas/balance.schema.js
+++ server/src/schemas/balance.schema.js
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const createbalanceSchema = z.object({
-  month: z.string({ required_error: "Date is required" }).datetime(),
+  month: z.string({ required_error: "Date is required" }).optional(),
   income: z.number().optional(),
   expenses: z.number().optional(),
 });
