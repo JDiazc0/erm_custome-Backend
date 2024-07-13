6cb34b025bcee2ec4628a82165d5c1a7b327b7f9
Jhoan Andres Diaz Castaño <115663029+JDiazc0@users.noreply.github.com>
2024-07-12 22:25:01 -0500

    Update balance.routes.js

--- server/src/routes/balance.routes.js
+++ server/src/routes/balance.routes.js
@@ -3,6 +3,7 @@ import {
   getBalances,
   getBalance,
   createBalance,
+  updateBalance,
 } from "../controllers/balance.controller.js";
 import { validateSchema } from "../middlewares/validator.middlewares.js";
 import { createbalanceSchema } from "../schemas/balance.schema.js";
@@ -12,5 +13,6 @@ const router = Router();
 router.get("/balance", getBalances);
 router.get("/balance/:id", getBalance);
 router.post("/balance", validateSchema(createbalanceSchema), createBalance);
+router.put("/balance/:id", updateBalance);
 
 export default router;
