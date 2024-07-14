bc42909a636e1b05ad8d0ad2fb25066474bdf38b
Jhoan Andres Diaz Castaño <115663029+JDiazc0@users.noreply.github.com>
2024-07-14 18:15:38 -0500

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ import balanceRouter from "./routes/balance.routes.js";
 const app = express();
 
 app.use(cors({
-    origin: 'https://mrdogkat.netlify.app/',
+    origin: 'https://mrdogkat.netlify.app',
     credentials: true
   }));  
 app.use(morgan("dev"));
